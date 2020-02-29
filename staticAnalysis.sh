ad05fd45bdedc5a959e5c90ecb143b3445f382de
Yattabyte <tklowry93@gmail.com>
2020-02-29 12:34:00 -0600

    0.9.7
    - Removed failure cases from static analysis tests from travis CI
      - Reason being that previous tests indicate state of linux build, whereas static analysis test is for convenience and shouldn't reflect ability to compile under gcc/clang on linux

--- staticAnalysis.sh
+++ staticAnalysis.sh
@@ -5,27 +5,27 @@ echo "
 **************************************************
 Starting CPPCheck
 **************************************************"
-cppcheck src tests -iexternal/ -I src/ -I tests/ --enable=all --quiet --suppress=missingIncludeSystem || exit 1
+cppcheck src tests -iexternal/ -I src/ -I tests/ --enable=all --quiet --suppress=missingIncludeSystem
 
 # Run Clang-Tidy using cmake
 echo "
 **************************************************
 Starting Clang-Tidy
 **************************************************"
-cmake -DBUILD_TESTING=ON -DCODE_COVERAGE=OFF -DSTATIC_ANALYSIS=OFF -DCMAKE_BUILD_TYPE=Debug -DCMAKE_CXX_CLANG_TIDY=clang-tidy-9 . || exit 1
-cmake --build . --clean-first -- -j $(nproc) || exit 1
-ctest --output-on-failure --quiet -j $(nproc) -C Debug . || exit 1
+cmake -DBUILD_TESTING=ON -DCODE_COVERAGE=OFF -DSTATIC_ANALYSIS=OFF -DCMAKE_BUILD_TYPE=Debug -DCMAKE_CXX_CLANG_TIDY=clang-tidy-9 .
+cmake --build . --clean-first -- -j $(nproc)
+ctest --output-on-failure --quiet -j $(nproc) -C Debug .
 
 # Run Valgrind
 echo "
 **************************************************
 Starting Memory Sanitizer (valgrind)
 **************************************************"
-cmake -DSTATIC_ANALYSIS=ON -UCMAKE_CXX_CLANG_TIDY . || exit 1
-cmake --clean-first . || exit 1
-ctest --output-on-failure --quiet -j $(nproc) -C Debug -D ExperimentalBuild . || exit 1
-ctest --output-on-failure --quiet -j $(nproc) -C Debug -D ExperimentalTest . || exit 1
-ctest --output-on-failure --quiet -j $(nproc) -C Debug -D ExperimentalMemCheck . || exit 1
+cmake -DSTATIC_ANALYSIS=ON -UCMAKE_CXX_CLANG_TIDY .
+cmake --clean-first .
+ctest --output-on-failure --quiet -j $(nproc) -C Debug -D ExperimentalBuild .
+ctest --output-on-failure --quiet -j $(nproc) -C Debug -D ExperimentalTest .
+ctest --output-on-failure --quiet -j $(nproc) -C Debug -D ExperimentalMemCheck .
 echo "Dumping Logs"
 echo "$(<${TRAVIS_BUILD_DIR}/Testing/Temporary/MemoryChecker.1.log)"
 
@@ -34,33 +34,33 @@ echo "
 **************************************************
 Starting Address Sanitizer
 **************************************************"
-cmake -DSTATIC_ANALYSIS=OFF -DCMAKE_CXX_FLAGS="-g -O0 -fno-omit-frame-pointer -fno-optimize-sibling-calls -fsanitize=address" -DCMAKE_EXE_LINKER_FLAGS="-g -O0 -fno-omit-frame-pointer -fno-optimize-sibling-calls -fsanitize=address" . || exit 1
-cmake --build . --clean-first -- -j $(nproc) || exit 1
-ctest --output-on-failure --quiet -j $(nproc) -C Debug . || exit 1
+cmake -DSTATIC_ANALYSIS=OFF -DCMAKE_CXX_FLAGS="-g -O0 -fno-omit-frame-pointer -fno-optimize-sibling-calls -fsanitize=address" -DCMAKE_EXE_LINKER_FLAGS="-g -O0 -fno-omit-frame-pointer -fno-optimize-sibling-calls -fsanitize=address" .
+cmake --build . --clean-first -- -j $(nproc)
+ctest --output-on-failure --quiet -j $(nproc) -C Debug .
 
 # Undefined Behaviour Sanitizer
 echo "
 **************************************************
 Starting Undefined-Behaviour Sanitizer
 **************************************************"
-cmake -DCMAKE_CXX_FLAGS="-g -O0 -fno-omit-frame-pointer -fno-optimize-sibling-calls -fsanitize=undefined" -DCMAKE_EXE_LINKER_FLAGS="-g -O0 -fno-omit-frame-pointer -fno-optimize-sibling-calls -fsanitize=undefined" . || exit 1
-cmake --build . --clean-first -- -j $(nproc) || exit 1
-ctest --output-on-failure --quiet -j $(nproc) -C Debug . || exit 1
+cmake -DCMAKE_CXX_FLAGS="-g -O0 -fno-omit-frame-pointer -fno-optimize-sibling-calls -fsanitize=undefined" -DCMAKE_EXE_LINKER_FLAGS="-g -O0 -fno-omit-frame-pointer -fno-optimize-sibling-calls -fsanitize=undefined" .
+cmake --build . --clean-first -- -j $(nproc)
+ctest --output-on-failure --quiet -j $(nproc) -C Debug .
 
 # Thread Sanitizer
 echo "
 **************************************************
 Starting Thread Sanitizer
 **************************************************"
-cmake -DCMAKE_CXX_FLAGS="-g -O0 -fno-omit-frame-pointer -fno-optimize-sibling-calls -fsanitize=thread" -DCMAKE_EXE_LINKER_FLAGS="-g -O0 -fno-omit-frame-pointer -fno-optimize-sibling-calls -fsanitize=thread" . || exit 1
-cmake --build . --clean-first -- -j $(nproc) || exit 1
-ctest --output-on-failure --quiet -j $(nproc) -C Debug . || exit 1
+cmake -DCMAKE_CXX_FLAGS="-g -O0 -fno-omit-frame-pointer -fno-optimize-sibling-calls -fsanitize=thread" -DCMAKE_EXE_LINKER_FLAGS="-g -O0 -fno-omit-frame-pointer -fno-optimize-sibling-calls -fsanitize=thread" .
+cmake --build . --clean-first -- -j $(nproc)
+ctest --output-on-failure --quiet -j $(nproc) -C Debug .
 
 # OCLint quality reporting
 echo "
 **************************************************
 Starting OCLint
 **************************************************"
-cmake -DCMAKE_CXX_FLAGS="-g -O0" -DCMAKE_EXE_LINKER_FLAGS="-g -O0" -DCMAKE_EXPORT_COMPILE_COMMANDS=ON . || exit 1
-cmake --build . --clean-first -- -j $(nproc) || exit 1
+cmake -DCMAKE_CXX_FLAGS="-g -O0" -DCMAKE_EXE_LINKER_FLAGS="-g -O0" -DCMAKE_EXPORT_COMPILE_COMMANDS=ON .
+cmake --build . --clean-first -- -j $(nproc)
 oclint-json-compilation-database -i src -i tests -e external
